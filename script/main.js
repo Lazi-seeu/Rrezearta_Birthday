d5cf9bb58d3bdce57607ab03fce6609e5742dfdd
Lazi Tefiki <it31081@seeu.edu.mk>
2025-11-03 18:14:07 +0100

    Fix cancel button text in confirmation dialog

--- script/main.js
+++ script/main.js
@@ -7,7 +7,7 @@ window.addEventListener('load', () => {
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
         confirmButtonText: 'Yes',
-        cancelButtonText: 'No',
+        cancelButtonText: 'Yes',
     }).then((result) => {
         if (result.isConfirmed) {
             document.querySelector('.song').play();
